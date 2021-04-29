3e8fff16a3afd2f9d2713e1248ca6d9ffa2dcf4b
Nidhin <nidhin@softwareassociates.in>
2021-04-29 17:56:49 +0530

    sample code

--- student_details.py
+++ student_details.py
@@ -1,40 +1,37 @@
+import json
+
+# Definig a class student
 class Student:
 
-    def __init__(self,name,standard,division,rollno,passeddata,faileddata,misseddata):
-        self.name = input("Enter your name:")
-        self.standard = int(input("Enter your class:"))
-        self.division = input("Enter your division:")
-        self.rollno = int(input("Enter your roll number:"))
-        self.passedtest = int(input("Enter passed test details:"))
-        self.failedtest = int(input("Enter failed test details:"))
-        self.missedtest = int(input("Enter missed test details:"))
+    
+    # method is defined to add student details
+    def add(self):
+        
+        print("-----------------------------")
+        print("-----------------------------")
+        print("Enter the Details:")
+        print("-----------------------------")
+        print("-----------------------------")
+        self.name = input("Name:")
+        self.standard = int(input("Class:"))
+        self.division = input("Division:")
+        self.rollno = int(input("Roll No:"))
+        self.passedtest = int(input("Passed test details:"))
+        self.failedtest = int(input("Failed test details:"))
+        self.missedtest = int(input("Missed test details:"))
+        aDict = {"Name:":self.name,"Class:":self.standard ,"Division:": self.division,"Roll No:":self.rollno,"Passed test details :":self.passedtest,"Failed test details:":self.failedtest, "Missed test details":self.missedtest}  
+        jsonString = json.dumps(aDict)
+        jsonFile = open("data.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
 
-class Test(Student):
-    
-    def menu(self):
-	    print("\n1. Add Details and Display")
-	    #  print("\n2. Display")
-	    print("\n2. Checking")
-	    print("\n3.Exit")
-	    try:
-	        enter = int(input("Please Select an Option: "))
-	    except ValueError:
-	        exit("\nHy! That's Not A Number")
-	    else:
-	        print("\n")
-	    if (enter == 1):
-	        display()
-	    elif (enter == 2):
-	        search()
-	    elif (enter == 3):
-	        print("Thank You!")
-	    elif(enter < 1 or enter > 3):
-	        print("Please Enter Valid Option")
-    
-    
-
+ # method is defined to display student details
     def display(self):
+        print("-----------------------------")
+        print("-----------------------------")
         print("Details are:")
+        print("-----------------------------")
+        print("-----------------------------")
         print("Name is : ", self.name)
         print("Class :", self.standard)
         print("Division:", self.division)
@@ -42,35 +39,61 @@ class Test(Student):
         print("No: of Passed Test:", self.passedtest)
         print("No: of Missed Test:", self.failedtest)
         print("No: of Failed Test:", self.missedtest)
-
+        
+        
+ # method is defined to check whether the student failed or missed more than 3 subjects
 
     def search(self):
+        print("-----------------------------")
+        print("-----------------------------")
+        print("Search Result is:")
+        print("-----------------------------")
+        print("-----------------------------")
         if(self.missedtest>=4 or self.failedtest>=4):
             print("Need Instructor")
         else:
-	        print("Study well!")
-	        
-ls =[]
-obj = Test('', 0,'', 0, 0, 0, 0)
-print("\nOperations used, ")
-print("\n1.Add and Display Student details\n2.Check Student Pass or Fail details\n3.Exit")    
- 
-try:
-    enter = int(input("Please Select an Option: "))
-except ValueError:
-    exit("\n Wrong Entry. Enter Again")
-else:
-    print("\n")    
-    
-if (enter == 1):
-    obj.display()
-elif (enter == 2):
-	obj.search()
-elif (enter == 3):
-	print("Thank You!")
-elif(enter < 1 or enter > 3):
-	print("Please Enter Correctly")   
-    
-    
-    
+            print("Study well!")
+            
 
+# Main Code
+
+# a list ls is created
+ls =[]
+
+#object obj for student class is created
+obj = Student()
+
+# displaying the opertions to be performed
+print("\nOperations used:")
+print("\n1.Add Student details\n2. Display Student details\n3.Check Student Pass or Fail details\n4.Exit")    
+
+
+x = 'y'
+while x == 'y':
+    
+    # it will check whether the option selected is not wrong
+    try:
+        enter = int(input("Please Select an Option: "))
+    except ValueError:
+        exit("\n Wrong Entry. Enter Again")
+    else:
+        print("\n") 
+    
+ # for each option given by user, it will perform the action.
+    if (enter == 1):
+        obj.add()
+    elif (enter == 2):
+        obj.display()
+    elif (enter == 3):
+        obj.search()
+    elif (enter == 4):
+        print("Thank You!")
+    elif(enter < 1 or enter > 4):
+        print("Please Enter Correctly") 
+    x = input("\nDo you want to continue? y/n: ")
+    
+# Printing the list of student
+n = 1
+for el in ls:
+    print(n,'. ', el)
+    n = n + 1
